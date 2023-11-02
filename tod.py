8d964b8090f584d23da524b6795960d2a6f99936
deathcore333 <amossmwangii@gmail.com>
2023-11-02 04:42:42 +0300

    added a new list of tod

--- tod.py
+++ tod.py
@@ -22,7 +22,19 @@ dare_list = [
     'Give a sensual massage to the person sitting next to you',
     'Do a body shot off of someone',
     'Give a hickey to the person you find most attractive in the room',
-    'Spend 5 minutes in a closet with the person of your choice'
+    'Spend 5 minutes in a closet with the person of your choice',
+    'Give someone in the room a sensual lap dance.',
+    'Kiss the person on your left passionately.',
+    'Remove an article of clothing and wear it in a provocative way.',
+    'Send a flirty or provocative text to someone you\'re attracted to.',
+    'Give a detailed description of your last sexual encounter.',
+    'Use an ice cube to tease and pleasure a volunteer.',
+    'Create an erotic story or poem and share it with the group.',
+    'Share your best seductive pick-up line.',
+    'Role-play a steamy scene with another player.',
+    'Blindfold one person and let them guess who\'s kissing them.',
+    'Perform a sensual striptease for the group.',
+    'Share a naughty photo (with your consent and within legal boundaries) with someone you trust.'
     ]
 
 truth_list = [
@@ -43,7 +55,19 @@ truth_list = [
     'What is the most sensitive part of your body?',
     'Have you ever had sex in public?',
     'What is your favorite type of porn?',
-    'What is your biggest sexual fear?'
+    'What is your biggest sexual fear?',
+    'Have you ever had a one-night stand? Share the details.',
+    'What\'s the most adventurous place you\'ve had sex?',
+    'Describe your favorite sexual fantasy.',
+    'Have you ever used a sex toy? Which one is your favorite?',
+    'What\'s the kinkiest thing you\'ve ever done in the bedroom?',
+    'Have you ever watched or read adult content in a public place?',
+    'Share your most embarrassing sexual moment.',
+    'Who is your celebrity crush, and what would you do if you met them?',
+    'Have you ever had a threesome? Share the experience.',
+    'What\'s your secret turn-on that most people wouldn\'t expect?',
+    'Share your wildest and most unforgettable sexual experience.',
+    'Have you ever had a romantic or sexual encounter with someone of the same gender?'
     ]
 previous_dares = []
 previous_truths = []
